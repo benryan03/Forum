451a569a9c114bea84e89fca48efcc1a90a4dbb9
Ben Ryan <benryan03@gmail.com>
2020-03-22 20:36:24 -0400

    Page can now display thread comments

--- view_thread.php
+++ view_thread.php
@@ -65,18 +65,40 @@
     </div>
 
     <div class="content">
+
+        
         <?php     
+            //Display thread OP
             echo nl2br(
                 "TITLE: ".$thread_array[2].
                 " REPLIES: ".$thread_array[5].
                 " AUTHOR: ".$thread_array[4]."\n\n".
                 "SUBMITTED AT: ".date_format($thread_array[6], "Y/m/d h:i:sa").
                 " UPDATED AT: ".date_format($thread_array[7], "Y/m/d h:i:sa\n\n").
-                "CONTENT: ".$thread_array[3]
+                "CONTENT: ".$thread_array[3]."\n\n"
             );
-        ?>
 
-        <br><br>
+            //Display comments
+
+            //Count how many comments are in the the thread
+            $query = "SELECT * FROM posts WHERE thread_id = '$thread_id' ORDER BY date_submitted";
+            $comments_array = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
+            $posts_count = sqlsrv_num_rows($comments_array);
+            
+            //Display each comment
+            for ($x = 1; $x < $posts_count + 1; $x++){
+                $comment_array_row = sqlsrv_fetch_array($comments_array, SQLSRV_FETCH_NUMERIC); //Select next row                
+
+                echo nl2br(
+                    "-----\n".
+                    "POST ID:".$comment_array_row[0].
+                    " AUTHOR: ".$comment_array_row[3].
+                    " SUBMITTED AT: ".date_format($comment_array_row[4], "Y/m/d h:i:sa\n\n").
+                    " COMMENT TEXT: ".$comment_array_row[2].
+                    "\n\n"
+                );
+            }
+        ?><br><br>
 
         <form action="?thread_id=<?php echo $thread_id ?>&<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
             <textarea name="comment_text" rows="4" cols="50" placeholder="Add comment" value="<?php echo htmlentities($thread_text) ?>"></textarea><br><br>
