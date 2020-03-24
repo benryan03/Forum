1a2f884b547c01e0b78cca039908a3074b328b4f
Ben Ryan <benryan03@gmail.com>
2020-03-24 13:22:01 -0400

    Added input validation for new_thread

--- new_thread.php
+++ new_thread.php
@@ -8,37 +8,58 @@ if (!isset($_SESSION["loggedInUser"])){
 
 $thread_title = "";
 $thread_text = "";
+$title_error = "";
+$text_error = "";
 date_default_timezone_set("America/New_York");
 $timestamp = date("m/d/Y h:ia");
+$errorStatus = false;
 
 $newPostID = 1; //I will change this later
 
 if (!empty($_POST["submit"])){
 
-    //Still need to sanitize inputs
-
-    //Retreive submitted thread title and text
+    //Retreive and sanitize submitted thread title and text
     $thread_title = htmlspecialchars($_POST["thread_title"]);
     $thread_text = htmlspecialchars($_POST["thread_text"]);
 
-    //Connect to database
-    $serverName = "localhost\sqlexpress";
-    $connectionInfo = array("Database"=>"Forum", "UID"=>"ben", "PWD"=>"password123");
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    //Validate thread title and text
+    if ($thread_title == ""){
+        $title_error = "Error: Title cannot be empty";
+        $errorStatus = true;
+    }
+    if (strlen($thread_title) > 50){
+        $title_error = "Error: Maximum length 50 characters (current: ".strlen($thread_title).")";
+        $errorStatus = true;
+    }
+    if ($thread_text == ""){
+        $text_error = "Error: Text cannot be empty";
+        $errorStatus = true;
+    }
+    if (strlen($thread_text) > 1000){
+        $text_error = "Error: Maximum length 1000 characters (current: ".strlen($thread_text).")";
+        $errorStatus = true;
+    }
+   
+    if ($errorStatus == false) { //No errors - submit to database
 
-    //To calculate new thread ID, count number of rows in database and add 1
-    $countExistingThreadsQuery = "SELECT * FROM threads";
-    $countExistingThreads = sqlsrv_query($conn, $countExistingThreadsQuery, array(), array( "Scrollable" => 'static' ));
-    $threads_count = sqlsrv_num_rows( $countExistingThreads );
-    $newThreadID = $threads_count + 1;
+        //Connect to database
+        $serverName = "localhost\sqlexpress";
+        $connectionInfo = array("Database"=>"Forum", "UID"=>"ben", "PWD"=>"password123");
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-    //Write new thread to database
-    $userRegisterQuery = "INSERT INTO threads VALUES ('$newThreadID', '$newPostID', '$thread_title', '$thread_text', '$_SESSION[loggedInUser]', '0', '$timestamp', '$timestamp')";
-    $writeToDatabase = sqlsrv_query($conn, $userRegisterQuery);
+        //To calculate new thread ID, count number of rows in database and add 1
+        $countExistingThreadsQuery = "SELECT * FROM threads";
+        $countExistingThreads = sqlsrv_query($conn, $countExistingThreadsQuery, array(), array( "Scrollable" => 'static' ));
+        $threads_count = sqlsrv_num_rows( $countExistingThreads );
+        $newThreadID = $threads_count + 1;
 
-    //Open success page
-    header("Location:new_thread_success.php");
+        //Write new thread to database
+        $userRegisterQuery = "INSERT INTO threads VALUES ('$newThreadID', '$newPostID', '$thread_title', '$thread_text', '$_SESSION[loggedInUser]', '0', '$timestamp', '$timestamp')";
+        $writeToDatabase = sqlsrv_query($conn, $userRegisterQuery);
 
+        //Open success page
+        header("Location:new_thread_success.php");
+    }
 }
 
 ?>
@@ -66,9 +87,15 @@ if (!empty($_POST["submit"])){
     <div class="content">
         New thread<br><br>
         <form action="? echo $_SERVER["PHP_SELF"]" method="post">
-        <input type="text" name="thread_title" placeholder="Thread title" value="<?php echo htmlentities($thread_title) ?>"><br><br>
-        <textarea name="thread_text" rows="4" cols="50" placeholder="Thread text" value="<?php echo htmlentities($thread_text) ?>"></textarea><br><br>
+
+        <input type="text" name="thread_title" placeholder="Thread title" value="<?php echo $thread_title ?>"><br>
+        <div class="error" id="title_error"><?php echo ($title_error) ?></div><br>
+
+        <textarea name="thread_text" rows="4" cols="50" placeholder="Thread text"><?php echo $thread_text ?></textarea><br>
+        <div class="error" id="text_error"><?php echo ($text_error) ?></div><br>
+
         <input type="submit" value="Submit" name="submit"><br>        
+        
         </form>
     </div>
 
