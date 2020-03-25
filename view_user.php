8101a7e5cc4aca5ecb3a52d011a9aaa7c257de09
Ben Ryan <benryan03@gmail.com>
2020-03-25 14:00:13 -0400

    Display posts on view_user.php

--- view_user.php
+++ view_user.php
@@ -6,6 +6,21 @@
 
     date_default_timezone_set("America/New_York");
     $timestamp = date("m/d/Y h:ia");
+
+    //Connect to database
+    $serverName = "localhost\sqlexpress";
+    $connectionInfo = array("Database"=>"Forum", "UID"=>"ben", "PWD"=>"password123");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    //Query database for number of threads posted by user
+    $query = "SELECT * FROM threads WHERE author = '$selectedUser'";
+    $threadsArray = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
+    $thread_count = sqlsrv_num_rows($threadsArray);
+
+    //Query database for number of comments posted by user
+    $query = "SELECT * FROM posts WHERE post_author = '$selectedUser'";
+    $commentsArray = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
+    $comment_count = sqlsrv_num_rows($commentsArray);
 ?>
 
 <html>
@@ -29,35 +44,41 @@
     </div>
 
     <div class="content">
-        <?php echo "Viewing profile of: ".$selectedUser; ?><br>
         <?php
-
-            //Connect to database
-            $serverName = "localhost\sqlexpress";
-            $connectionInfo = array("Database"=>"Forum", "UID"=>"ben", "PWD"=>"password123");
-            $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-            //Query database for number of threads posted by user
-            $query = "SELECT * FROM threads WHERE author = '$selectedUser'";
-            $result = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
-            $thread_count = sqlsrv_num_rows($result);
-
-            //Query database for number of comments posted by user
-            $query = "SELECT * FROM posts WHERE post_author = '$selectedUser'";
-            $result = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
-            $comment_count = sqlsrv_num_rows($result);
-
+            echo nl2br("Viewing profile of: ".$selectedUser."\n");
             echo nl2br("User has created ".$thread_count." threads.\n");
-            echo nl2br("User has posted ".$comment_count." comments.\n");
-    
-    
-    
-    
-    
-    
-    
-    
-    
+            echo nl2br("User has posted ".$comment_count." comments.");
+        ?>
+    </div>
+    <div class="view_user_threads">
+        Threads:<br>
+        <?php
+            //Display threads
+            for ($x = 1; $x < $thread_count + 1; $x++){
+                $thread_array_row = sqlsrv_fetch_array($threadsArray, SQLSRV_FETCH_NUMERIC); //Select next row                
+                echo nl2br(
+                    //Do not change the next 2 lines or all formatting will break :)
+                    '<div class="complete_thread"><span class="thread_title">'."<a href='view_thread.php?thread_id=$thread_array_row[0]'>$thread_array_row[2]</a></span><span class='thread_details'>replies: $thread_array_row[5] by: <a href='view_user.php?selectedUser=$thread_array_row[4]'>$thread_array_row[4]</a></span>
+                    </div>"
+                );    
+            }
+        ?>
+    </div>
+    <div class="view_user_comments">
+        Comments:<br>
+        <?php
+            //Display comments
+            for ($x = 1; $x < $comment_count + 1; $x++){
+                $comment_array_row = sqlsrv_fetch_array($commentsArray, SQLSRV_FETCH_NUMERIC); //Select next row                
+
+                echo nl2br(
+                    "<h2><i>post id:".$comment_array_row[0].
+                    " | submitted: ".date_format($comment_array_row[4], "m/d/Y h:ia")."</i></h2>".
+                    $comment_array_row[2].
+                    "\n\n"
+                );
+            }
+
         ?>
     </div>
 
