523e2f1caeb7b33a3a6d5834898e4b8dac04229a
Ben Ryan <benryan03@gmail.com>
2020-03-27 16:55:32 -0400

    Cleaned up formatting

--- view_thread.php
+++ view_thread.php
@@ -1,16 +1,15 @@
  <?php
     session_start();
 
+    //If no user is logged in, setLoggedInUser to None
+    if (!isset($_SESSION["loggedInUser"])){
+        $_SESSION["loggedInUser"] = "None";}
+
     date_default_timezone_set("America/New_York");
     $timestamp = date("m/d/Y h:ia");
     $comment_text = "";
     $comment_error = "";
     $errorStatus = false;
-
-    //If no user is logged in, setLoggedInUser to None
-    if (!isset($_SESSION["loggedInUser"])){
-        $_SESSION["loggedInUser"] = "None";
-    }
 
     //Get thread ID from previous link
     $thread_id = $_GET['thread_id'];
@@ -27,28 +26,23 @@
 
     //For editing posts
     if (isset($_GET['editClicked'])){
-        $post_id = $_GET['post_id'];
+        $post_id = $_GET['post_id'];}
 
-    }
+    if (!empty($_POST['submit'])){//When new comment is submitted
 
-    if (!empty($_POST['submit'])){
-
-        //Retrieve and sanitize submitted comment
+        //Retrieve and sanitize comment
         $comment_text = htmlspecialchars($_POST['comment_text']);
 
         //Validate comment
         if ($comment_text == ""){
             $comment_error = "Error: Comment cannot be empty";
-            $errorStatus = true;
-        }
+            $errorStatus = true;}
         if (strlen($comment_text) > 1000){
             $comment_error = "Error: Maximum length 1000 characters (current: ".strlen($comment_text).")";
-            $errorStatus = true;
-        }
+            $errorStatus = true;}
 
-        if ($errorStatus == false){
-            //Write comment to database
-                
+        if ($errorStatus == false){ //Write comment to database
+
             //To calculate new comment ID, count number of rows in database and add 1
             $countExistingPostsQuery = "SELECT * FROM posts";
             $countExistingPosts = sqlsrv_query($conn, $countExistingPostsQuery, array(), array( "Scrollable" => 'static' ));
@@ -59,43 +53,40 @@
             $newPostQuery = "INSERT INTO posts VALUES ('$newPostID', '$thread_id', '$comment_text', '$_SESSION[loggedInUser]', '$timestamp', '$timestamp', '0')";
             $writeToDatabase = sqlsrv_query($conn, $newPostQuery);
     
-            //Query number of existing comments
+            //Query number of existing comments (for thread comment count number in index.php)
             $replyCountQuery = "SELECT reply_count FROM threads WHERE thread_id = '$thread_id' ";
             $reply_count = sqlsrv_query($conn, $replyCountQuery, array());
             $reply_count = sqlsrv_fetch_array($reply_count);
             $reply_count = $reply_count[0];
     
-            //Update comment count and date updated for thread in threads database
+            //Update comment count and date updated for thread
             $reply_count = $reply_count + 1;
             $threadUpdateQuery = "UPDATE threads SET reply_count = '$reply_count', time_updated = '$timestamp' WHERE thread_id = '$thread_id' ";
             $writeToDatabase = sqlsrv_query($conn, $threadUpdateQuery);
         }
     }
 
-    if (!empty($_POST['submit_edit'])){
+    if (!empty($_POST['submit_edit'])){ //When comment edit is submitted
 
-        //Retrieve and sanitize submitted comment
+        //Retrieve and sanitize the edit
         $edit_text = htmlspecialchars($_POST['edit_text']);
         $edited_post_id = $_GET['edited_post_id'];
 
-        //Validate comment
+        //Validate edit
         if ($edit_text == ""){
             $comment_error = "Error: Comment cannot be empty";
-            $errorStatus = true;
-        }
+            $errorStatus = true;}
         if (strlen($edit_text) > 1000){
             $comment_error = "Error: Maximum length 1000 characters (current: ".strlen($edit_text).")";
-            $errorStatus = true;
-        }
+            $errorStatus = true;}
 
-        if ($errorStatus == false){
-            //Write edit to database
+        if ($errorStatus == false){ //Write edit to database
             $editPostQuery = "UPDATE posts SET post_text = '$edit_text', date_updated = '$timestamp', edited_status = '1' WHERE post_id = '$edited_post_id'";
             $writeToDatabase = sqlsrv_query($conn, $editPostQuery);
         }
     }
 
-    print_r(sqlsrv_errors());
+    print_r(sqlsrv_errors()); //Print SQL errors if any occur - I still need to move this into an error div
 ?>
 
 <html>
@@ -120,13 +111,6 @@
 
     <div class="op">
         <?php     
-
-            /*
-            $editLink = "";
-            if ($_SESSION["loggedInUser"] == trim($thread_array[4])){
-                $editLink = " | <a href='?thread_id=$thread_id&post_id='op'[0]&editClicked=true'>Edit</a>";
-            }*/
-        
             //Display thread OP
             echo nl2br(
                 "<h2>".$thread_array[2].
@@ -134,16 +118,12 @@
                 " by: <a href='view_user.php?selectedUser=$thread_array[4]'>".trim($thread_array[4])."</a>"." | ".
                 "submitted: ".date_format($thread_array[6], "m/d/Y h:ia").
                 //$editLink.
-                "</i></h2>".$thread_array[3]."\n"
-            );
+                "</i></h2>".$thread_array[3]."\n");
         ?>
     </div>
  
     <div class="comments">
         <?php
-
-            //Display comments
-
             //Count how many comments are in the the thread
             $query = "SELECT * FROM posts WHERE thread_id = '$thread_id' ORDER BY date_submitted";
             $comments_array = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
@@ -156,8 +136,7 @@
                 //Define edit link for each comment, with individual comment ID
                 $editLink = "";
                 if ($_SESSION["loggedInUser"] == trim($comment_array_row[3])){
-                    $editLink = " | <a href='?thread_id=$thread_id&post_id=$comment_array_row[0]&editClicked=true'>Edit</a>";
-                }
+                    $editLink = " | <a href='?thread_id=$thread_id&post_id=$comment_array_row[0]&editClicked=true'>Edit</a>";}
 
                 //Display comment metadata
                 echo nl2br(
@@ -166,14 +145,13 @@
                     " | submitted: ".date_format($comment_array_row[4], "m/d/Y h:ia").
                     $editLink."</i>\n");
 
-                if ($comment_array_row[6] == "1"){ //If comment has been edited
-                    echo "<b>Edited at ".date_format($comment_array_row[5], "m/d/Y h:ia")."</b></h2>";
-                }
+                //If comment has been edited, display datetime of update
+                if ($comment_array_row[6] == "1"){
+                    echo "<b>Edited at ".date_format($comment_array_row[5], "m/d/Y h:ia")."</b></h2>";}
                 else{
-                    echo "</h2>";
-                }
+                    echo "</h2>";}
 
-                //Need to make sure that correct user has clicked the edit link
+                //Still need to make sure that correct user has clicked the edit link
 
                 //If edit link has been clicked, display text box to edit comment
                 if (isset($_GET['post_id']) && $_GET['post_id'] == $comment_array_row[0] && isset($_GET['editClicked'])){
@@ -183,15 +161,12 @@
                         htmlentities(trim($comment_array_row[2])).'</textarea><br>'.
                         '<div class="error" id="comment_error">'.$comment_error.'</div><br>'.
                         '<input type="submit" value="Submit" name="submit_edit">'.
-                        '</form>'
-                    );
-                }
+                        '</form>');}
                 else{
-                    echo nl2br(trim($comment_array_row[2])."\n\n");
-                }   
+                    echo nl2br(trim($comment_array_row[2])."\n\n");}   
             }
-        ?><br><br>
-
+        ?>
+        <br><br>
         <form action="?thread_id=<?php echo $thread_id ?>&<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
             <textarea name="comment_text" rows="4" cols="50" placeholder="Add comment"><?php echo htmlentities($comment_text) ?></textarea><br>
             <div class="error" id="comment_error"><?php echo ($comment_error) ?></div><br>
